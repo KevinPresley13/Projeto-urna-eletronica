9f793462e0014df16473c856fb0239c92e552bf7
Kevin Presley <kevinpresley1995@gmail.com>
2022-06-15 18:14:17 -0300

    CRIANDO ARRAY de candidatos

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numero:5,
+        candidatos:[
+            {
+                numero:22222,
+                nome:'Zezinho',
+                partido:'ABC',
+                fotos:[
+                    {url:'assets/images/caricatura1.png',legenda:'vereador'}
+                ]
+            },
+            {
+                numero:33333,
+                nome:'Pedrinho',
+                partido:'FGH',
+                fotos:[
+                    {url:'assets/images/caricatura1.png',legenda:'vereador'}
+                ]
+            }
+            
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numero:2,
+        candidatos:[
+            {
+                numero:22,
+                nome:'Jose',
+                partido:'ABC',
+                fotos:[
+                    {url:'assets/images/caricatura1.png',legenda:'PREFEITO'},
+                    {url:'assets/images/caricatura1.png',legenda:'VICE-PREFEITO',small: true}
+                ]
+            },
+            {
+                numero:33,
+                nome:'Pedro',
+                partido:'FGH',
+                fotos:[
+                    {url:'assets/images/caricatura1.png',legenda:'PREFEITO'},
+                    {url:'assets/images/caricatura1.png',legenda:'VICE-PREFEITO',small: true}
+                ]
+            }
+            
+        ]
+    }
+];
