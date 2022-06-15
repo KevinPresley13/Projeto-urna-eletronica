03f75e18e0060376c3a17b12e47ce9aa044e15f7
Kevin Presley <kevinpresley1995@gmail.com>
2022-06-15 19:50:40 -0300

    adicionando funcionalidades nos botoeseinformaçoes

---
+++ script.js
@@ -0,0 +1,19 @@
+let seuVotoPara = document.querySelector('.linha-1 span');
+let cargo = document.querySelector('.linha-2 span');
+let descriçao = document.querySelector('.linha-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.foto');
+let numeros = document.querySelector('.linha-3');
+
+function clicou(n) {
+    alert(n);
+};
+function branco() {
+
+};
+function corrige() {
+
+};
+function confirma() {
+
+};
